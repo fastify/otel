cef799a50dbf1798cf059a672b9227a5d36dc746
Onur Temizkan <onur@narval.co.uk>
2025-03-31 12:55:09 +0100

    chore(types): Vendor in `FastifyInstance` type

--- test/index.test-d.ts
+++ test/index.test-d.ts
@@ -2,13 +2,20 @@ import { expectAssignable } from 'tsd'
 import { InstrumentationBase, InstrumentationConfig } from '@opentelemetry/instrumentation'
 import { fastify as Fastify } from 'fastify'
 
-import { FastifyOtelInstrumentation, FastifyOtelInstrumentationOpts } from '..'
+import { FastifyOtelInstrumentation, FastifyOtelInstrumentationOpts, FastifyInstance, FastifyPlugin } from '..'
 
 expectAssignable<InstrumentationBase>(new FastifyOtelInstrumentation())
 expectAssignable<InstrumentationConfig>({ servername: 'server', enabled: true } as FastifyOtelInstrumentationOpts)
 expectAssignable<InstrumentationConfig>({} as FastifyOtelInstrumentationOpts)
 
 const app = Fastify()
+const plugin = new FastifyOtelInstrumentation().plugin()
+
+expectAssignable<FastifyInstance>(app)
+expectAssignable<FastifyPlugin>(plugin)
+expectAssignable<FastifyInstance>(app.register(plugin))
+expectAssignable<FastifyInstance>(app.register(plugin).register(plugin))
+
 app.register(new FastifyOtelInstrumentation().plugin())
 app.register((nested, _opts, done) => {
   nested.register(new FastifyOtelInstrumentation().plugin())
