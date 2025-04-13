fdcf91eb7f85cde81488835046f0aee3c2dc91a6
Carlos Fuentes <me@metcoder.dev>
2025-04-13 11:51:14 +0200

    test: add testing coverage

--- test/api.test.js
+++ test/api.test.js
@@ -1,10 +1,11 @@
 'use strict'
 
 const { test, describe } = require('node:test')
-const assert = require('assert')
+const assert = require('node:assert')
 const Fastify = require(process.env.FASTIFY_VERSION || 'fastify')
 
 const { InstrumentationBase } = require('@opentelemetry/instrumentation')
+const { trace, context } = require('@opentelemetry/api')
 
 const FastifyInstrumentation = require('..')
 
@@ -37,5 +38,37 @@ describe('Interface', () => {
     app.register(plugin)
 
     await app.ready()
+  })
+
+  test('FastifyInstrumentation#plugin should expose the right set of APIs', async t => {
+    /** @type {import('fastify').FastifyInstance} */
+    const app = Fastify()
+    const instrumentation = new FastifyInstrumentation()
+    const plugin = instrumentation.plugin()
+
+    t.plan(9)
+
+    await app.register(plugin)
+
+    app.get('/', (request, reply) => {
+      const otel = request.opentelemetry()
+
+      t.assert.equal(typeof otel.span.spanContext().spanId, 'string')
+      t.assert.equal(typeof otel.tracer, 'object')
+      t.assert.equal(typeof otel.context, 'object')
+      t.assert.equal(typeof otel.inject, 'function')
+      t.assert.equal(otel.inject.length, 2)
+      t.assert.ok(!otel.inject({}))
+      t.assert.equal(typeof otel.extract, 'function')
+      t.assert.equal(otel.extract.length, 2)
+      t.assert.equal(typeof (otel.extract({})), 'object')
+
+      return 'world'
+    })
+
+    await app.inject({
+      method: 'GET',
+      url: '/'
+    })
   })
 })
